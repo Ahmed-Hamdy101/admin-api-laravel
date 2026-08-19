23dacf6e44f9b07cc5c27ce07aec093801e3304f
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-08-19 16:45:16 +0300

    feat: add comment to clarify chart data retrieval for the last 7 days

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,11 +10,12 @@ class DashboardController extends Controller
 {
       public function chart(Request $request)
     {
-        // 
+        // show chart data for the last 7 days
         $order= Order::query()
         ->join('order_items', 'orders.id', '=', 'order_items.order_id')
         ->selectRaw("DATE(orders.created_at) as date, SUM(order_items.price * order_items.quantity) as sum")
             ->groupBy('date')
+            ->where('orders.created_at', '>=', now()->subDays(7))
             ->get();
 
         return ChartResources::collection($order);
